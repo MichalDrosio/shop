4ae8bf37b74e3db884eaa1483f5a2798b7934d8e
mordziak <drosio.michal@gmail.com>
2020-04-15 13:05:37 +0200

    cart

---
+++ cart/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.cart_detail, name='c'),
+    path('add/<int:product_id>/', views.cart_add, name='card_add'),
+    path('remove/<int:product_id>', views.cart_remove, name = 'cart_remove'),
+]
