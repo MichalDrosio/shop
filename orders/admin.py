f98c817d5733338c2ef910b8ae8fcbe53dc14b9b
mordziak <drosio.michal@gmail.com>
2020-04-15 14:45:27 +0200

    orders

---
+++ orders/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Order, OrderItem
+# Register your models here.
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'adress', 'postal_code', 'city', 'paid', 'created', 'update']
+    list_filter = ['paid', 'created', 'update']
+    inlines = [OrderItemInline]
+
+admin.site.register(Order, OrderAdmin)
